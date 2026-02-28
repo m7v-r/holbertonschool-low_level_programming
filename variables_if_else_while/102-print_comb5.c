e757376936d20acadc4944d56b35632c36751173
m7v-r <ms4571690@icloud.com>
2026-02-28 13:55:35 +0000

    Add task 12: final combinations of two-digit numbers

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			/* طباعة الرقم الأول (خانتين) */
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+
+			putchar(' '); /* مسافة بين الرقمين */
+
+			/* طباعة الرقم الثاني (خانتين) */
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			/* شرط طباعة الفاصلة والمسافة (إلا عند الوصول لآخر مجموعة 98 99) */
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
