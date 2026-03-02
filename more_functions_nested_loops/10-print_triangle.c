3e896ee6e8a5ce35e201efcf767b3356e146c7ca
m7v-r <ms4571690@icloud.com>
2026-03-02 19:16:12 +0000

    Add print_triangle function

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			/* طباعة المسافات */
+			for (j = 0; j < (size - i); j++)
+			{
+				_putchar(' ');
+			}
+			/* طباعة الرموز # */
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
