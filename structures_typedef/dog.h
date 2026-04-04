475770317a6e1d15ed95f5ab5ac84f53e58c9e90
m7v-r <ms4571690@icloud.com>
2026-04-04 13:37:38 +0000

    Add dog_t typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,8 +16,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-#endif /* DOG_H */
+#endif /* DOG_H *
