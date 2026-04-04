3f0487bce5806aeb37f772eada0343912aa162a7
m7v-r <ms4571690@icloud.com>
2026-04-04 12:54:35 +0000

    Add _calloc function

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array and sets it to zero
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory, or NULL if fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i, total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	/* Set memory to zero */
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
