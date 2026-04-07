5f39f75ac6618711f1a93db89397ab096def73d7
m7v-r <ms4571690@icloud.com>
2026-04-07 14:47:05 +0000

    Add function_pointers.h and task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name using a given function
+ * @name: name of the person
+ * @f: pointer to the function to use for printing
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
