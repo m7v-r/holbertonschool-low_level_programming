5ac5f459cee229f2c781d94d2339e4af102fe1d6
m7v-r <ms4571690@icloud.com>
2026-04-08 19:43:50 +0000

    Fix file distribution for task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,31 +1,7 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
 
-/**
- * get_op_func - selects the correct function to perform the operation
- * @s: operator passed as argument
- *
- * Return: pointer to the function that corresponds to the operator
- */
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
-	}
-
-	return (NULL);
-}
+int op_add(int a, int b) { return (a + b); }
+int op_sub(int a, int b) { return (a - b); }
+int op_mul(int a, int b) { return (a * b); }
+int op_div(int a, int b) { return (a / b); }
+int op_mod(int a, int b) { return (a % b); }
