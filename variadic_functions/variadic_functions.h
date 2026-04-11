d109808b8a343414c4c4319e493d0b9002ca5fd8
m7v-r <ms4571690@icloud.com>
2026-04-11 17:00:55 +0000

    Add task 2: print_strings function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,15 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
+#endif /* VARIADIC_FUNCTIONS_H */#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
 #endif /* VARIADIC_FUNCTIONS_H */
