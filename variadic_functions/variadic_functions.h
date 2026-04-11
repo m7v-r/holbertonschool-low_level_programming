798872e95afb492b162f64f17d6c7406c5ac8973
m7v-r <ms4571690@icloud.com>
2026-04-11 16:58:28 +0000

    Fix syntax error in header file include guards

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,14 +2,6 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-
-#endif /* VARIADIC_FUNCTIONS_H */#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-#include <stdarg.h>
 #include <stdio.h>
 
 int _putchar(char c);
