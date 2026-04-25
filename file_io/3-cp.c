2d5c70c357f4cabab2b86ad5a91ee15a59882ba6
m7v-r <ms4571690@icloud.com>
2026-04-25 19:49:33 +0000

    Refactor cp program to comply with Betty 40-line rule

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,19 @@
 #include <unistd.h>
 
 /**
+ * close_fd - Closes a file descriptor and exits on failure.
+ * @fd: The file descriptor to be closed.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
@@ -21,52 +34,36 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* Permissions: rw-rw-r-- (0664) */
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from);
+		close_fd(file_from);
 		exit(99);
 	}
-
 	while ((n_read = read(file_from, buffer, 1024)) > 0)
 	{
 		n_written = write(file_to, buffer, n_read);
 		if (n_written == -1 || n_written != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
+			close_fd(file_from);
+			close_fd(file_to);
 			exit(99);
 		}
 	}
-
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-
+	close_fd(file_from);
+	close_fd(file_to);
 	return (0);
 }
