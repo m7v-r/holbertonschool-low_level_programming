033d430e74e1354001773ea074c640a4614cb6dc
m7v-r <ms4571690@icloud.com>
2026-04-25 19:18:40 +0000

    Add task 7: insert_dnodeint_at_index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index where the new node should be added, starts at 0
+ * @n: integer to be stored in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *temp = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (temp && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (!temp && idx > i)
+		return (NULL);
+
+	if (temp->next == NULL && idx == i + 1)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = temp->next;
+	new->prev = temp;
+
+	if (temp->next)
+		temp->next->prev = new;
+
+	temp->next = new;
+
+	return (new);
+}
