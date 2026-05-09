5488abc1179f63e140c8b0ca248c38ae389b863d
m7v-r <ms4571690@icloud.com>
2026-05-09 15:39:42 +0000

    Fix Betty: Split function to reduce length

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,10 +1,44 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - Creates a new hash node.
+ * @key: The key for the node.
+ * @value: The value for the node.
+ *
+ * Return: Pointer to the new node, or NULL on failure.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table to add or update the key/value to.
+ * @ht: The hash table to add or update.
  * @key: The key. Cannot be an empty string.
- * @value: The value associated with the key. Must be duplicated.
+ * @value: The value associated with the key.
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
@@ -12,45 +46,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *temp;
 	unsigned long int index;
-	char *value_copy;
+	char *new_value;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-
-	/* Check if key already exists to update value */
 	temp = ht->array[index];
+
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
 			free(temp->value);
-			temp->value = value_copy;
+			temp->value = new_value;
 			return (1);
 		}
 		temp = temp->next;
 	}
 
-	/* Create new node if key doesn't exist */
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
-	{
-		free(value_copy);
 		return (0);
-	}
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		free(value_copy);
-		return (0);
-	}
-	new_node->value = value_copy;
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
